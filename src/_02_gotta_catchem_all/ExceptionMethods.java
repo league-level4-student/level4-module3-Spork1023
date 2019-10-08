2568841909bc8323c011b346e9df9d0afb5f547f
league <league@imac-10.attlocal.net>
2019-10-08 16:34:48 -0700

    Finsihed module 3 level 4

--- src/_02_gotta_catchem_all/ExceptionMethods.java
+++ src/_02_gotta_catchem_all/ExceptionMethods.java
@@ -1,5 +1,28 @@
 package _02_gotta_catchem_all;
 
 public class ExceptionMethods {
+
+	public Double divide(double x, double y) {
+		Double ret;
+		if(y == 0.0) {
+			throw new IllegalArgumentException("y cant be 0");
+		}
+		else {
+			ret = x*y;
+		}
+		return ret;
+	}
 	
+	public String reverseString(String str) {
+		String str2 = "";
+		if(str.contentEquals("")) {
+			throw new IllegalStateException("String cant be empty");
+		}
+		else {
+		for(int i = str.length(); i > 0; i++) {
+			str2 = str2 + str.charAt(i);
+		}		
+		}
+		return str2;
+	}
 }
